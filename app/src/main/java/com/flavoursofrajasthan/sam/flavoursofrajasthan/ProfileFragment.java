96d085a004dc043dd7993195db4e87d4f5511916
ershamsadkhan <ershamsadkhan@gmail.com>
2017-06-06 00:30:28 -0700

    Added Profile

--- app/src/main/java/com/flavoursofrajasthan/sam/flavoursofrajasthan/ProfileFragment.java
+++ app/src/main/java/com/flavoursofrajasthan/sam/flavoursofrajasthan/ProfileFragment.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -32,15 +36,13 @@ import retrofit2.Response;
  */
 
 public class ProfileFragment extends Fragment {
-
-
-    public CustomListAdapter sta;
-
+    Button saveBtn;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.profile, container, false);
     }
 
@@ -49,6 +51,36 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
+
+        saveBtn=(Button)getActivity().findViewById(R.id.btn_save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "btnCLicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.edit) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
